affd735af14c9be2e954240d44b6fa3b8d72db46
serenalilian <serenalilian99@gmail.com>
2024-04-16 21:22:39 +0000

    get students

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,6 @@
-import getListStudents from './0-get_list_students';
-
-export default function getStudentsByLocation(student = getListStudents(), city) {
-  return student.filter((el) => el.location === city);
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  }
+  return [];
 }
-
